9a205f3d0f179bc9e30c69f54cf4bd5f97e71928
Vlad Brebu <vlad@Vlads-MacBook-Pro.local>
2026-05-06 11:14:13 +0100

    initial terraform setup

---
+++ main.tf
@@ -0,0 +1,68 @@
+resource "aws_vpc" "main" {
+    cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public"{
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.0.0/24"
+    map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "igw"{
+    vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public_rt"{
+    vpc_id = aws_vpc.main.id
+    route {
+        cidr_block="0.0.0.0/0"
+        gateway_id = aws_internet_gateway.igw.id
+    }
+}
+
+resource "aws_route_table_association" "public_assoc"{
+    subnet_id = aws_subnet.public.id
+    route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_security_group" "sg"{
+    vpc_id = aws_vpc.main.id
+
+    ingress{
+        from_port = 22
+        to_port = 22
+        protocol ="tcp"
+        cidr_blocks = ["149.13.192.33/32"]
+    }
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol ="tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress{
+        from_port = 0 
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_instance" "ubuntu"{
+    ami = "ami-091138d0f0d41ff90"
+    instance_type = "t3.large"
+    subnet_id = aws_subnet.public.id
+    vpc_security_group_ids = [aws_security_group.sg.id]
+    associate_public_ip_address = true
+    tags = {
+        Name = "ubuntu-devops"
+    }
+    key_name = aws_key_pair.devops.key_name
+}
+
+resource "aws_key_pair" "devops" {
+    key_name = "devops-key"
+    public_key = file("~/.ssh/devops-key.pub")
+}
