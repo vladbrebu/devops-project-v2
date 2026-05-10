7eff55954a3e61296c5c24cf83a962b11c842fe0
Vlad Brebu <vlad@Vlads-MacBook-Pro.local>
2026-05-10 18:10:27 +0100

    enable remote state

--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
 provider "aws" {
     region = "us-east-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "vladbrebu-devops-tf-state-54821"
+    key = "terraform/state.tfstate"
+    region = "us-east-1"
+  }
+}
