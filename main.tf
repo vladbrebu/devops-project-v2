b89c2833f36d62fa406c7a1573182076f53c9e46
Vlad Brebu <vlad@Vlads-MacBook-Pro.local>
2026-05-13 10:41:20 +0100

    added ingress rule for port 9100

--- main.tf
+++ main.tf
@@ -42,6 +42,13 @@ resource "aws_security_group" "sg"{
         cidr_blocks = ["0.0.0.0/0"]
     }
 
+       ingress {
+        from_port = 9100
+        to_port = 9100
+        protocol ="tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
     egress{
         from_port = 0 
         to_port = 0
