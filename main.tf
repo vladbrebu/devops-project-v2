d45fcc510b45bc78d62bd33e7dd352318e70a76c
Vlad Brebu <vlad@Vlads-MacBook-Pro.local>
2026-05-13 11:29:40 +0100

    added ingress rules for prometheus and grafana

--- main.tf
+++ main.tf
@@ -49,6 +49,20 @@ resource "aws_security_group" "sg"{
         cidr_blocks = ["0.0.0.0/0"]
     }
 
+       ingress {
+        from_port = 9090
+        to_port = 9090
+        protocol ="tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+           ingress {
+        from_port = 3000
+        to_port = 3000
+        protocol ="tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    
     egress{
         from_port = 0 
         to_port = 0
